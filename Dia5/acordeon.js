df9ee787211af1fa6004957e63588b316bbb35c7
gerardomdn95 <gerardo.mdn95@gmail.com>
2019-07-02 19:48:30 -0500

    piedra papel tijeras

---
+++ Dia5/acordeon.js
@@ -0,0 +1,7 @@
+   let element = document.getElementById("myDIV");
+   element.classList.add("mystyle");
+
+   document.getElementById().innerHTML('Hola')
+
+   <p id="demo" onclick="myFunction()">Click me to change my HTML content (innerHTML).</p>
+   document.getElementById("demo").innerHTML = "Paragraph changed!";
