f998d4b6b2c2fbcc07eb9a4656cb858e84fead20
gerardomdn95 <gerardo.mdn95@gmail.com>
2019-07-15 19:43:23 -0500

    mi commit desde feature/binary

---
+++ Binary/binary.js
@@ -0,0 +1,16 @@
+function decimalToBinary(a) {
+    let binary = []
+    while (a > 0) {
+        if (a % 2 == 0) {
+            binary.push("0")
+            a = a / 2
+        } else {
+            binary.push("1")
+            a = a / 2
+
+            a = Math.trunc(a)
+        }
+    }
+    binary.reverse()
+    return binary.join("")
+}
